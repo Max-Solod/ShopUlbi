ba7babdb1f79903179375830bcdf124434781fa8
Solo Max <maxsolod163@yande.ru>
2024-08-05 16:05:12 +0400

    1

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
-a = 2;
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => { console.log(`server started on PORT:${PORT}`) })
